15b2c6eb59ddbf74f10063dad5b5f2cf44b790fc
lifechat <lifechat@126.com>
2022-03-12 16:19:00 +0800

    fit(修改bug)

--- foodie-dev-api/src/main/java/com/immoc/controller/StuFooController.java
+++ foodie-dev-api/src/main/java/com/immoc/controller/StuFooController.java
@@ -7,12 +7,11 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class StuFooController {
 
-//    @Autowired()
-//    private StuService stuService;
+    @Autowired()
+    private StuService stuService;
 
     @RequestMapping(value = "/getStu",method = RequestMethod.GET)
     public Object getStu(int id){
-//        return stuService.getStuInfo(id);
-        return "FWJEOF";
+        return stuService.getStuInfo(id);
     }
 }
